701c2f7da0d847a11eedd29df30abb1779982fc1
Wiktor Czetyrbok <kasztanators2701@gmail.com>
2023-03-20 15:53:46 +0100

    basic clasess

--- code/map.py
+++ code/map.py
@@ -1,5 +1,15 @@
 import pygame
 from support import import_folder
-#class Map:
- #   def __init__(self):
-  #     self.backround =
+from settings import *
+
+
+class Map:
+    def __init__(self):
+        self.bg_img = pygame.image.load("../assets/images/background/background.jpg").convert_alpha()
+
+    def draw_background(self, screen):
+        scaled_bg = pygame.transform.scale(self.bg_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.blit(scaled_bg, (0, 0))
+
+    def refresh(self, screen):
+        self.draw_background(screen)
