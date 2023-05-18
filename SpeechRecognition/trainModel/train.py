eeb692c9001084e6c4a558cbaef83d1f1152e92f
Wiktor Czetyrbok <88346002+kasztanators@users.noreply.github.com>
2023-05-18 11:42:08 +0200

    dividing code into files, refactoring

--- SpeechRecognition/trainModel/train.py
+++ SpeechRecognition/trainModel/train.py
@@ -4,6 +4,8 @@ import numpy as np
 import seaborn as sns
 import tensorflow as tf
 from IPython import display
+from helper import get_spectrogram
+from exportModel import ExportModel
 
 # Set the seed value for experiment reproducibility.
 seed = 42
@@ -77,24 +79,10 @@ for i in range(n):
 
 plt.show()
 
-
-def get_spectrogram(waveform):
-    # Convert the waveform to a spectrogram via a STFT.
-    spectrogram = tf.signal.stft(
-        waveform, frame_length=255, frame_step=128)
-    # Obtain the magnitude of the STFT.
-    spectrogram = tf.abs(spectrogram)
-    # Add a `channels` dimension, so that the spectrogram can be used
-    # as image-like input data with convolution layers (which expect
-    # shape (`batch_size`, `height`, `width`, `channels`).
-    spectrogram = spectrogram[..., tf.newaxis]
-    return spectrogram
-
-
 for i in range(3):
     label = label_names[example_labels[i]]
     waveform = example_audio[i]
-    spectrogram = get_spectrogram(waveform)
+    spectrogram = get_spectrogram(waveform, label_names)
 
     print('Label:', label)
     print('Waveform shape:', waveform.shape)
@@ -132,7 +120,7 @@ plt.show()
 
 def make_spec_ds(ds):
     return ds.map(
-        map_func=lambda audio, label: (get_spectrogram(audio), label),
+        map_func=lambda audio, label: (get_spectrogram(audio, label_names), label),
         num_parallel_calls=tf.data.AUTOTUNE)
 
 
@@ -242,61 +230,28 @@ def plot_command(command_name, file_dest):
     x, sample_rate = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000, )
     x = tf.squeeze(x, axis=-1)
     waveform = x
-    x = get_spectrogram(x)
+    x = get_spectrogram(x, label_names)
     x = x[tf.newaxis, ...]
 
     prediction = model(x)
-    x_labels = ['down','go', 'left','right', 'stop', 'up', 'yes']
+    x_labels = ['down', 'go', 'left', 'right', 'stop', 'up', 'yes']
     plt.bar(x_labels, tf.nn.softmax(prediction[0]))
     plt.title(command_name)
     plt.show()
 
 
-plot_command('Yes','yes/0ab3b47d_nohash_0.wav')
-plot_command('Up','up/0ab3b47d_nohash_0.wav')
-plot_command('Stop','stop/0b40aa8e_nohash_0.wav')
-plot_command('Right','right/0ab3b47d_nohash_0.wav')
-plot_command('Left','left/0b09edd3_nohash_0.wav')
+plot_command('Yes', 'yes/0ab3b47d_nohash_0.wav')
+plot_command('Up', 'up/0ab3b47d_nohash_0.wav')
+plot_command('Stop', 'stop/0b40aa8e_nohash_0.wav')
+plot_command('Right', 'right/0ab3b47d_nohash_0.wav')
+plot_command('Left', 'left/0b09edd3_nohash_0.wav')
 plot_command('Go', 'go/0a9f9af7_nohash_0.wav')
 plot_command('Down', 'down/0a9f9af7_nohash_0.wav')
 
-
-
 display.display(display.Audio(waveform, rate=16000))
 
+export_model = ExportModel(model, label_names)
 
-class ExportModel(tf.Module):
-    def __init__(self, model):
-        self.model = model
-
-        # Accept either a string-filename or a batch of waveforms.
-        # YOu could add additional signatures for a single wave, or a ragged-batch.
-        self.__call__.get_concrete_function(
-            x=tf.TensorSpec(shape=(), dtype=tf.string))
-        self.__call__.get_concrete_function(
-            x=tf.TensorSpec(shape=[None, 16000], dtype=tf.float32))
-
-    @tf.function
-    def __call__(self, x):
-        # If they pass a string, load the file and decode it.
-        if x.dtype == tf.string:
-            x = tf.io.read_file(x)
-            x, _ = tf.audio.decode_wav(x, desired_channels=1, desired_samples=16000, )
-            x = tf.squeeze(x, axis=-1)
-            x = x[tf.newaxis, :]
-
-        x = get_spectrogram(x)
-        result = self.model(x, training=False)
-
-        class_ids = tf.argmax(result, axis=-1)
-        class_names = tf.gather(label_names, class_ids)
-        return {'predictions': result,
-                'class_ids': class_ids,
-                'class_names': class_names}
-
-
-export = ExportModel(model)
-
-tf.keras.models.save_model(export.model, "../../code/saved_model/saved")
+tf.keras.models.save_model(export_model.model, "../../code/saved_model/saved")
 imported = tf.keras.models.load_model("../../code/saved_model/saved")
-#imported(waveform[tf.newaxis, :])
+# imported(waveform[tf.newaxis, :])
